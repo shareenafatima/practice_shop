544472f4054ce51964f58b5a9f278a0481a65ffa
shareena fatima <shareenafatima40@gmail.com>
2024-04-01 00:21:29 +0500

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controller: {
+    registrations: 'registrations'
+  }
   root "store#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
